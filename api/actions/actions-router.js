341078bc1096d414163fcedffd355f938e91be79
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-09 10:21:38 -0500

    Endpoints and routers scaffolded. Server also structured.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,21 @@
 // Write your "actions" router here!
+
+const express = require('express')
+
+const Actions = require('./actions-model')
+
+// const { } = require('./actions-middleware')
+
+const router = express.Router()
+
+router.get('/api/actions', (req, res) => {})
+
+router.get('/api/actions/:id', (req, res) => {})
+
+router.post('/api/actions', (req, res) => {})
+
+router.put('/api/actions/:id', (req, res) => {})
+
+router.delete('/api/actions/:id', (req, res) => {})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
 // Write your "projects" router here!
+
+const express = require('express')
+
+const Projects = require('./projects-model')
+
+// const { } = require('./projects-middleware')
+
+const router = express.Router()
+
+router.get('/api/projects', (req, res) => {})
+
+router.get('/api/projects/:id', (req, res) => {})
+
+router.post('/api/projects', (req, res) => {})
+
+router.put('/api/projects/:id', (req, res) => {})
+
+router.delete('/api/projects/:id', (req, res) => {})
+
+router.get('/api/projects/:id/actions', (req, res) => {})
+
+module.exports = router
